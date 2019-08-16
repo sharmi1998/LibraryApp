45cd102817e779340b2f41a9b6a44e94436a6ab7
sharmi1998 <gayu438@gmail.com>
2019-08-16 14:54:00 +0530

    work

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const Express=require('express')
 var app=new Express();
 app.set('view engine','ejs');
+
 app.use(Express.static(__dirname+"/public"));
+
 app.get('/',(req,res)=>{
-    res.render('index')
-});
+    res.render(
+        'index',
+        {
+            navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'}],
+
+            title:'Library'
+        }
+    )
+})
 app.listen(process.env.PORT || 3004,()=>{
     console.log("server is running on http://localhost:3004")
     });
