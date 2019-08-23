f55741ac4cf233886b2f306c742c57c8f70c917a
sharmi1998 <gayu438@gmail.com>
2019-08-23 18:15:16 +0530

    update

--- app.js
+++ app.js
@@ -1,20 +1,30 @@
-const Express=require('express')
-var bodyParser=require('body-parser')
-var app=new Express()
-var request=require('request')
-const Mongoose=require('mongoose');
-app.set('view engine','ejs')
-app.use(Express.static(__dirname+"/public"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
-const   LibModel=Mongoose.model("libraries",{
+const Express=require('express');
+var app=new Express();
+app.set('view engine','ejs');
+var request=require('request');
+var Mongoose=require('mongoose');
+var bodyparser=require('body-parser');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+var LibModel=Mongoose.model("library",{
     title:String,
     author:String,
     publisher:String,
-    image:String
-    
-    
+    date_of_publication:String,
+    distributor:String,
+    price:String,
+    description:String,
+    pic:String
+
 });
+var AuthorModel=Mongoose.model("libraryAuthor",{
+    name:String,
+    place:String,
+    dob:String,
+    image:String
+});
+
+app.use(Express.static(__dirname+"/public"));
 //Mongoose.connect("mongodb://localhost:27017/library")
 
 Mongoose.connect("mongodb+srv://sharmi1998:sharmi1998@cluster0-rwxfj.mongodb.net/test?retryWrites=true&w=majority")
@@ -30,11 +40,63 @@ app.post('/read',(req,res)=>{
          
         }
         else{
+            console.log(data)
             res.send("<script>alert('successfully added')</script>        <script> window.location.href='/' </script>");
         }
     })
 
 })
+
+app.post('/read1',(req,res)=>
+{
+    var author=new AuthorModel(req.body);
+    var result=author.save((error)=>{
+        if(error)
+        {
+            throw error;
+        }
+        else{
+            res.send("<script> window.location.href='/addauthor' </script>       <script>alert('successfully added')</script>" );
+        }
+    });
+  
+});
+
+// app.post('/read1',(req,res)=>{
+
+//     // console.log('test')
+//     // console.log(req.body)
+//     var author=new AuthorModel(req.body);
+//     var result=author.save((error,data)=>{
+//         if(error){
+//             throw error;
+         
+//         }
+//         else{
+//             res.send("<script>alert('successfully added')</script>        <script> window.location.href='/' </script>");
+//         }
+//     })
+
+// })
+
+app.get('/read1',(req,res)=>
+{
+    AuthorModel.find((error,data)=>
+{
+    if(error)
+    {
+        throw error;
+    }
+    else{
+        res.send(data);
+    }
+});
+
+});
+
+
+
+
 
 app.get('/viewall',(req,res)=>{
 
@@ -48,13 +110,6 @@ app.get('/viewall',(req,res)=>{
     })
     
     })
-
-
-
-
-
-
-
 // app.get('/viewbook',(req,res)=>{
 //     request(viewall,(error,response,body)=>{
 //         var data=JSON.parse(body);
@@ -64,112 +119,27 @@ app.get('/viewall',(req,res)=>{
     
 //     })
 
-   
-
-
-
-
-// books=[
-//     {
-//         'title':'In search of Lost Time',
-//         'author':'Marcel Proust',
-//         'publisher':'HarperCollins',
-//         'date_of_publication':'1971',
-//         'distributorName':'HarperCollins',
-//         'price':340,
-//         'Description':'Novel'
-
-
-//     },
-//     {
-//         title:"Don Quixote",
-//         author:"Miguel de Cervantes",
-//         publisher:"H&C Books",
-//         date_of_publication:"1940",
-//         distributorName:"H&C Books",
-//         price:840,
-//         Description:"Real World story"
-
-//     },
-//     {
-//         title:"Ulysses",
-//         author:"James Joyces",
-//         publisher:"H&C Books",
-//         date_of_publication:"1930",
-//         distributorName:"H&C Books",
-//         price:450,
-//         Description:"Story"
-//     },
-//     {
-//             title:"The Great Gatsby",
-//             author:"F.schoot",
-//             publisher:"Pearson",
-//             date_of_publication:"1959",
-//             distributorName:"H&C Books",
-//             price:740,
-//             Description:'Novel'
-    
-//     },
-//     {
-//             title:"MobyDick",
-//             author:"Herman Melville",
-//             publisher:"Pearson",
-//             date_of_publication:"1851",
-//             distributorName:"H&C Books",
-//             price:200,
-//             Description:'AutoBiography'
-    
-//         },
-//         {
-//             title:"Hamlet",
-//             author:"William Shakesphere",
-//             publisher:"pearson Books",
-//             date_of_publication:"1920",
-//             distributorName:"H&C Books",
-//             price:1090,
-//             Description:"Poems"
-//         },];
-//         a=[
-//             {
-//                 AuthorName:'marcel proust',
-//                 image: '/image/a.jpg',
-//                 dob:'12-12-1870',
-//                 place:'america'
-//             },
-//             {
-//                 AuthorName:'Don Quixote',
-//                 image:"/image/f.jpg",
-//                 dob:'12-09-1870',
-//                 place:'america'
-//             },
-//             {
-//                 AuthorName:'James Joyces',
-//                 image:"/image/b.jpg",
-//                 dob:'10-09-1870',
-//                 place:'europe'
-//             },{
-//                 AuthorName:'F.schoot',
-//                 image:"/image/c.jpg",
-//                 dob:'10-09-1860',
-//                 place:'singapore'
-//             },{
-//                 AuthorName:'Herman Melville',
-//                 image:"/image/d.jpg",
-//                 dob:'1-09-1879',
-//                 place:'new york'
-//             },{
-//                 AuthorName:'William Shakesphere',
-//                 image:"/image/e.jpg",
-//                 dob:'11-03-1871',
-//                 place:'london'
-//             },
-//         ];
-
 app.get('/singlebookFetchApi/:id',(req,res)=>{
 
     var id=req.params.id;
 
-    result=LibModel.find({_id:id},(error,data)=>{
+    LibModel.find({_id:id},(error,data)=>{
+        if(error){
+            throw error;
+        }
+        else{
+
+            res.send(data);
+        }
+    })
+
+})
+
+app.get('/authorFetchApi/:id',(req,res)=>{
+
+    var id=req.params.id;
+
+    AuthorModel.find({_id:id},(error,data)=>{
         if(error){
             throw error;
         }
@@ -184,33 +154,46 @@ app.get('/singlebookFetchApi/:id',(req,res)=>{
 
 app.get('/readmore/:id',(req,res)=>{
 
-    var id=req.params.id;
+    var x=req.params.id;
 
 
-    const ApiUrl="https://libmanage.herokuapp.com/singlebookFetchApi/"+id;
+    const ApiUrl="https://libmanage.herokuapp.com/singlebookFetchApi/"+x;
 
    
 request(ApiUrl,(error,response,body)=>{
     var books=JSON.parse(body);
     console.log(books)
-    res.render('single',{ navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/books',title:'viewbook'}
+    res.render('single',{ navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}
 ],title:'books',books:books[0]})
    // res.render('books',{data:'data'})
 })
 
 })
-
-
 app.get('/authors1/:id',(req,res)=>{
 
-    const x=req.params.id;
-    res.render('authors1',{'a': a[x],
+    var x=req.params.id;
 
-    navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/books',title:'viewbook'}],
 
-    title:'Library'}   )
+    const ApiUrl="https://libmanage.herokuapp.com/authorFetchApi/"+x;
+
+   
+request(ApiUrl,(error,response,body)=>{
+    var author=JSON.parse(body);
+    console.log(author)
+    res.render('authors1',{ navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}
+],title:'authors',"author":author[0]})
+   // res.render('books',{data:'data'})
+})
 
 })
+
+    // const x=req.params.id;
+    // res.render('authors1',{'a': a[x],
+
+    // navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}],
+
+    // title:'Library'}   )
+
 
 
 
@@ -219,7 +202,7 @@ app.get('/',(req,res)=>{
     res.render(
         'index',
         {
-            navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/books',title:'viewbook'}],
+            navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}],
 
             title:'Library'
         }
@@ -235,21 +218,58 @@ app.get('/books',(req,res)=>{
 request(viewall,(error,response,body)=>{
     var data=JSON.parse(body);
     console.log(data)
-    res.render('books',{ navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/books',title:'viewbook'}
-],title:'books',data})
+    res.render('books',{ navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}
+],title:'books',data:data})
    // res.render('books',{data:'data'})
 })
 
 })
 
+app.get('/author',(req,res)=>{
+   // const authorlink="http://localhost:3000/getauthorApi";
+   const authorlink="https://libmanage.herokuapp.com/getauthorApi"
+   
+    request(authorlink,(error,response,body)=>{
+        var a=JSON.parse(body);
+        if(error)
+        {
+            throw error;
+        }
+        else{
+            res.render('author',{ navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}
+    ],title:'author',a:a})
+        }
+        
+    });
+});
 
-app.get('/authors',(req,res)=>{
-    res.render('author',{ navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/books',title:'viewbook'}],title:'authors',a:a})
-})
+
+// app.get('/authors',(req,res)=>{
+
+
+
+//     request(authorlink,(error,response,body)=>{
+//         var authors=JSON.parse(body);
+//         // console.log(author)
+//         if(error){
+//             throw error;
+//         }
+//         else{
+//         res.render('author',{ navbarrr:[{link:'/books',title:'books'},{link:'/authors',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}
+//     ],title:'author',authors:authors})
+// }
+       // res.render('books',{data:'data'})
+
+    
+
+
+
 app.get('/addbook',(req,res)=>{
-    res.render('addb',{title:'addbooks'})
+    res.render('addbook',{navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}]})
 })
-
+app.get('/addauthor',(req,res)=>{
+    res.render('addauthor',{navbarrr:[{link:'/books',title:'books'},{link:'/author',title:'authors'},{link:'/addbook',title:'addbook'},{link:'/addauthor',title:'AddAuthor'}]})
+})
 app.listen(process.env.PORT || 3004,()=>{
     console.log("server is running on http://localhost:3004")
-    });
+})
